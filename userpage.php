b7b1d3534196db3cf65c702b1c25b04fd281a362
igenhimel <djstar.himel@gmail.com>
2021-02-08 00:10:23 +0600

    updated

--- userpage.php
+++ userpage.php
@@ -1,63 +1,14 @@
-<?php
-require_once("userpageheader.php");
-require_once("logout.php");
-?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+       <meta name="viewport" content="width=device-width , intial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link rel="stylesheet" href="Facebook.css">
 </head>
 <body>
-
-
-    <?php
-
-if(isset($_SESSION['first_name'])){
-
-
-echo "<h2 style='color:red'>";
-echo "Welcome To Your Profile ".$_SESSION['first_name'];
-echo "</h2> <br>";
-?>
-
-
-
-<div class="userprofile">
-<?php
-
-    echo "First Name: ".$_SESSION['first_name']."<br>";
-echo "<br>Surname: ".$_SESSION['last_name']."<br>";
-
-
-
-?>
-    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">  <input type="submit" value="Update" name="updateinfo">  </form>
-
-
-
-<?php
-}
-    else{
-        echo "<p class='meow'>You are not logged in<br>. Please Log in and try again</p>";
-
-
-
-    ?>
-     <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
-        <div class="home">
-         <input type="submit" value="Home" name="home">
-         </div>
-
-
-    <?php
-
-    }
-    ?>
-
-</div>
 
 
 
