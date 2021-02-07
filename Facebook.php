4178285b3bb8211f1b3cab89af824e9c6b89970a
igenhimel <djstar.himel@gmail.com>
2021-02-08 00:07:41 +0600

    updated

--- Facebook.php
+++ Facebook.php
@@ -3,6 +3,8 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width , intial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Facebook - log in or sign up</title>
     <link rel="shortcut icon" type="image/x-icon" href="assest/fb-icon.ico">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet">
